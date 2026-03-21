b83e4a6ca7999e904589bafd198ea046bc7d39d4
Zeos AI <alabeauai@gmail.com>
2026-03-20 21:46:20 -0500

    Use logo on login page and main page

--- poc-04-auth/server.js
+++ poc-04-auth/server.js
@@ -540,6 +540,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 // ---------------------------------------------------------------------------
 // Helpers
 // ---------------------------------------------------------------------------
